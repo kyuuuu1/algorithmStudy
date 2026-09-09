e03679ad5bdc566e4f7082a7247e88821b6c48fc
Hyun Kyuwon <hyunkw99@gmail.com>
2026-09-09 23:37:20 +0900

    뒤집힌 문자열, 문자 반복 출력하기, 배열 뒤집기, 아이스 아메리카노, 짝수 홀수 개수

---
+++ programmers/.idea/아이스아메리카노.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int money) {
+
+        int a = money/5500;
+        int b = money%5500;
+        int[] answer = {a, b};
+
+        return answer;
+    }
+}
